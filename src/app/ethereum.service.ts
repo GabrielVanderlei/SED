c1c29230d9b8d1d23f06dc8a128307758c18c079
GabrielVanderlei <gabrielvanderlei10@hotmail.com>
2019-05-13 00:19:50 -0300

    3 files modified

--- src/app/ethereum.service.ts
+++ src/app/ethereum.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
 export class EthereumService {
   
   public address:any;
-  public events:string="";
 
   constructor() { }
 
--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class EventService {
 
+  public events:any;
+
   constructor() { }
 
+  getEvents(){
+    this.events = "a";
+  }
 }
